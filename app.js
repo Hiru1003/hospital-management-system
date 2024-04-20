c73bce27ac759c79f2df6e4b007d77056eb364c6
Hiru1003 <hirumiperera10@gmail.com>
2024-04-20 10:39:10 +0530

    feat: connect to cloudinary

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 import express from "express";
-import {config} from "dotenv";
-
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import messageRouter from "./router/messageRouter.js";
 
 const app = express();
-config({path: "./config"})
+config({path: "./config/config.env"})
+
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+      method: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
+  );
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
+    })
+  );
+  app.use("/api/v1/message", messageRouter);
+
+  dbConnection();
 
 export default app;
