bb06b326a55f8ebef2a6e8ffe5b58763e7101716
Hiru1003 <hirumiperera10@gmail.com>
2024-04-21 18:06:25 +0530

    feat: login route

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   patientRegister,
+  login,
 } from "../controller/userController.js";
 
 
 const router = express.Router();
 
 router.post("/patient/register", patientRegister);
+router.post("/login", login);
 
 export default router;
