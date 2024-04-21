5bc269443fff5570ca51d1b1de88a2e89ce18f3c
Hiru1003 <hirumiperera10@gmail.com>
2024-04-21 18:06:15 +0530

    feat: login

--- controller/userController.js
+++ controller/userController.js
@@ -39,3 +39,31 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
     message: "User registed",
   })
 });
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password, confirmPassword, role } = req.body;
+    if (!email || !password || !confirmPassword || !role) {
+      return next(new ErrorHandler("Please Fill Full Form!", 400));
+    }
+    if (password !== confirmPassword) {
+      return next(
+        new ErrorHandler("Password & Confirm Password Do Not Match!", 400)
+      );
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid Email Or Password!", 400));
+    }
+  
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid Email Or Password!", 400));
+    }
+    if (role !== user.role) {
+      return next(new ErrorHandler(`User Not Found With This Role!`, 400));
+    }
+    res.status(200).json({
+        success: true,
+        message: "Login Successfully!",
+      })
+  });
