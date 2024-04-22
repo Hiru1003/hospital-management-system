ffdd1e5dfa68650664ef7d0a390b0649994e498c
Hiru1003 <hirumiperera10@gmail.com>
2024-04-22 09:26:05 +0530

    feat: cloudinary

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { User } from "../models/userSchema.js";
 import ErrorHandler from "../middlewares/error.js";
 import { generateToken } from "../utils/jwtToken.js";
+import cloudinary from "cloudinary";
 
 
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
