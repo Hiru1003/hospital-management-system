1a8ddf442501d1458a1a6ceaed7cd27da0d75e97
Hiru1003 <hirumiperera10@gmail.com>
2024-04-22 07:49:17 +0530

    feat: route for get all doc

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,13 +1,25 @@
 import express from "express";
 import {
   patientRegister,
+  addNewAdmin,
+  getAllDoctors,
+  getUserDetails,
   login,
 } from "../controller/userController.js";
+
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+} from "../middlewares/auth.js";
 
 
 const router = express.Router();
 
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
+router.post("/admin/addnew",isAdminAuthenticated, addNewAdmin);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+router.get("/patient/me", isPatientAuthenticated, getUserDetails);
+router.get("/doctors", getAllDoctors);
 
 export default router;
