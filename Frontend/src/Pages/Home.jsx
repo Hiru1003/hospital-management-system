f825c6a7042fe73a981fb00ddabb7ca0901c333c
Hiru1003 <hirumiperera10@gmail.com>
2024-04-25 16:47:12 +0530

    feat: import files

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
+import MessageForm from "../components/MessageForm";
+import Departments from "../components/Departments";
 
 const Home = () => {
   return (
     <>
-      <Hero
-        title={
+   <Hero>
+   title={
           "Welcome to ZeeCare Medical Institute | Your Trusted Healthcare Provider"
         }
-        imageUrl={"/hero.png"}
-      />
-      <Biography imageUrl={"/about.png"} />
-      <Departments />
-      <MessageForm />
-    </>
-  )
-}
+         imageUrl={"/hero.png"}
+   </Hero>
+   <Departments/>
+   <MessageForm/>
 
-export default Home
+    </>
+  );
+};
+
+export default Home;
