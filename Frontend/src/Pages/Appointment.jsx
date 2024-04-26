7cd820b9b7f262d52c042625defa07e3dc5e91fb
Hiru1003 <hirumiperera10@gmail.com>
2024-04-26 08:30:44 +0530

    feat: content

--- Frontend/src/Pages/Appointment.jsx
+++ Frontend/src/Pages/Appointment.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import AppointmentForm from "../components/AppointmentForm";
 
 const Appointment = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="hero container">
+        <div className="banner">
+          <h1>{"Schedule Your Appointment | ZeeCare Medical Institute"}</h1>
+          <p>
+            ZeeCare Medical Institute is a state-of-the-art facility dedicated
+            to providing comprehensive healthcare services with compassion and
+            expertise. Our team of skilled professionals is committed to
+            delivering personalized care tailored to each patient's needs. At
+            ZeeCare, we prioritize your well-being, ensuring a harmonious
+            journey towards optimal health and wellness.
+          </p>
+        </div>
+        <div className="banner">
+          <img src={"/signin.png"} alt="hero" className="animated-image" />
+          <span>
+            <img src="/Vector.png" alt="vector" />
+          </span>
+        </div>
+      </div>
+      <AppointmentForm/>
+    </>
+  );
+};
 
-export default Appointment
+export default Appointment;
