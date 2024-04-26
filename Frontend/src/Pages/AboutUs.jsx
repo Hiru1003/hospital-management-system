a600f6707d4993ef3ed34f33639b6ffdb3b74f5a
Hiru1003 <hirumiperera10@gmail.com>
2024-04-26 08:30:50 +0530

    feat: about us

--- Frontend/src/Pages/AboutUs.jsx
+++ Frontend/src/Pages/AboutUs.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
 const AboutUs = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="hero container">
+        <div className="banner">
+          <h1>{"Welcome to ZeeCare Medical Institute | Your Trusted Healthcare Provider"}</h1>
+          <p>
+            ZeeCare Medical Institute is a state-of-the-art facility dedicated
+            to providing comprehensive healthcare services with compassion and
+            expertise. Our team of skilled professionals is committed to
+            delivering personalized care tailored to each patient's needs. At
+            ZeeCare, we prioritize your well-being, ensuring a harmonious
+            journey towards optimal health and wellness.
+          </p>
+        </div>
+        <div className="banner">
+          <img src={"/about.png"} alt="hero" className="animated-image" />
+          <span>
+            <img src="/Vector.png" alt="vector" />
+          </span>
+        </div>
+      </div>
+      <Biography imageUrl={"/whoweare.png"} />
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
